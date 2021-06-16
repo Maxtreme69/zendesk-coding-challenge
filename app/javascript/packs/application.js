61ef92e0c5020a638b6255636dd76dd5100f08f3
Max <s3853476@student.rmit.edu.au>
2021-06-16 14:18:27 +0000

    added jquery, bootstrap, popper and navbar to rails application

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,36 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+//= require jquery
+//= require jquery_ujs
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require bootstrap
+//*= require_tree .
+
+
+// import 'bootstrap'
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
+// require("channels")
+// require("packs/custom")
+// require("packs/posts.js")
+
+import "bootstrap"
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+console.log('Hello from application.js')
+
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
